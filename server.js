c21406378807e03b42f340019936427e8c1071e4
kranthithalishetty341 <kranthithalishetty341@gmail.com>
2026-05-01 19:43:51 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/send-email', async (req, res) => {
+    const { name, email, mobile, subject, message } = req.body;
+
+    try {
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'kranthit.1997@gmail.com',
+                pass: 'xyjjorfccizyqxpx'
+            },
+            tls: {
+                rejectUnauthorized: false 
+            }
+        });
+
+        let mailOptions = {
+            from: email,
+            to: 'kranthit.1997@gmail.com',
+            subject: subject || 'New Contact Inquiry',
+            html: `
+        <h3>New Inquiry</h3>
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Mobile:</b> ${mobile}</p>
+        <p><b>Message:</b> ${message}</p>
+      `
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        res.status(200).json({ message: 'Email sent successfully' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error sending email' });
+    }
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
